eb0dc8590eb92a26a2151a99c1d1e7ffe033aad2
batikanor <batikanor@gmail.com>
2020-09-16 11:10:58 +0300

    Responsiveness improvementss

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 
 import Layout from '../components/layout'
