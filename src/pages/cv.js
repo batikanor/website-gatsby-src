0d506707f2e337eba04b3b1a7fd95b1d9b609613
batikanor <batikanor@gmail.com>
2020-10-10 15:43:59 +0300

    added a static image for quantum technologies workshop

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -26,13 +26,19 @@ const CVPage = ( props ) => {
 
         // Old cv from creddle
         //<iframe className={cvStyles.cv}  title="English CV" src="https://resume.creddle.io/embed/ijqcr0r5bf1" width="100%" height="100%" seamless></iframe>
+        
+
+        // Default embedded from Google Docs
+        //<h2 class={cvStyles.cvCaption}>English <a href="https://docs.google.com/document/d/e/2PACX-1vTI_n0Epv5KdWd5cO9d_78Lbvzvpb2gN7IqtrOPrYEpLHBd9islycKCqAk3BoDcH0fEOMzypmLvVQan/pub?embedded=true" target="_blank" rel="noreferrer"> (Click for fullscreen)</a> <a href="https://www.batikanor.com/l/cv/en" target="_blank" rel="noreferrer"> (Click for PDF)</a> </h2>
+
+
         <Layout>
             <Head title="CV"/>
             <h1>CV</h1>
             <CommentCount config={disqusConfig} placeholder={'...'} />
             <hr/>
             <p>My Resume in English, German and Turkish are accesible below. If you want a more detailed one please <Link to="/contact">Contact me!</Link> </p>
-            <h2 class={cvStyles.cvCaption}>English <a href="https://docs.google.com/document/d/e/2PACX-1vTI_n0Epv5KdWd5cO9d_78Lbvzvpb2gN7IqtrOPrYEpLHBd9islycKCqAk3BoDcH0fEOMzypmLvVQan/pub?embedded=true" target="_blank" rel="noreferrer"> (Click for fullscreen)</a> <a href="https://www.batikanor.com/l/cv/en" target="_blank" rel="noreferrer"> (Click for PDF)</a> </h2>
+            <h2 class={cvStyles.cvCaption}>English <a href="https://www.batikanor.com/l/cv/en" target="_blank" rel="noreferrer"> (Click for fullscreen)</a> <a href="https://www.batikanor.com/l/cv/en" target="_blank" rel="noreferrer"> (Click for PDF)</a> </h2>
             <div className={cvStyles.cvDiv}>
             <iframe className={cvStyles.cv} title="English CV" width="100%" height="100%" seamless src="https://docs.google.com/document/d/e/2PACX-1vTI_n0Epv5KdWd5cO9d_78Lbvzvpb2gN7IqtrOPrYEpLHBd9islycKCqAk3BoDcH0fEOMzypmLvVQan/pub?embedded=true"></iframe>
 
