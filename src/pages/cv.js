29cc2c8e77131df55deaf3e96123651abe8fce8c
batikanor <batikanor@gmail.com>
2020-10-11 04:07:28 +0300

    improved cv

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -38,9 +38,9 @@ const CVPage = ( props ) => {
             <CommentCount config={disqusConfig} placeholder={'...'} />
             <hr/>
             <p>My Resume in English, German and Turkish are accesible below. If you want a more detailed one please <Link to="/contact">Contact me!</Link> </p>
-            <h2 class={cvStyles.cvCaption}>English <a href="https://www.batikanor.com/l/cv/en" target="_blank" rel="noreferrer"> (Click for fullscreen)</a> <a href="https://www.batikanor.com/l/cv/en" target="_blank" rel="noreferrer"> (Click for PDF)</a> </h2>
-            <div className={cvStyles.cvDiv}>
-            <iframe className={cvStyles.cv} title="English CV" width="100%" height="100%" seamless src="https://docs.google.com/document/d/e/2PACX-1vTI_n0Epv5KdWd5cO9d_78Lbvzvpb2gN7IqtrOPrYEpLHBd9islycKCqAk3BoDcH0fEOMzypmLvVQan/pub?embedded=true"></iframe>
+            <h2 class={cvStyles.cvCaption}>English <a href="/l/cv/en/fullscreen" target="_blank" rel="noreferrer"> (Click for fullscreen)</a> <a href="/l/cv/en" target="_blank" rel="noreferrer"> (Click for PDF)</a> </h2>
+            <div className={cvStyles.cvDiv}> 
+            <iframe className={cvStyles.cv} title="English CV" width="100%" height="100%" seamless src="/l/cv/embed/en"></iframe>
 
             </div>
             <h2>German</h2>
